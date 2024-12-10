d56731ea183461bfb401b180f38513e15b7e921c
carolinapletti <carolina.pletti@gmail.com>
2024-12-10 08:53:23 +0100

    [update]: created first script. It only contains comments for now

---
+++ Cluster_based_main.m
@@ -0,0 +1,16 @@
+% permutation tests for fNIRS wavelet transform coherence data
+
+% for interaction data: irrespective of group, compare all participants
+% with random pairs (first calculate the random pairs)
+% difference between real pairs and averaged random permutation pairs will
+% tell us in which frequencies and time points there where differences
+% between real interacting participants and fake participants pairs -> so
+% it's a t-test, not an ANOVA!
+
+% for video data: compare participants watching videos together with
+% participants watching videos separately (since they are practically the
+% same as randomly permuted pairs - same stimuli but they don't even see
+% each other) -> again t-test, not ANOVA!
+
+% for interaction data:
+
