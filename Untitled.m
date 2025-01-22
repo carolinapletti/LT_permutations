b8d75b2cf0a9cd0c8f28e20df90ad23f7bdfeb5e
carolinapletti <carolina.pletti@gmail.com>
2025-01-22 15:33:22 +0100

    Major changes in functions, still work in progress

--- Untitled.m
+++ Untitled.m
@@ -4,7 +4,7 @@ numPlots = length(data_cell); % Number of subplots needed
 for i = 1:numPlots
     % Create a subplot for each matrix
     subplot(ceil(sqrt(numPlots)), ceil(sqrt(numPlots)), i); % Square grid layout
-    dataMatrix = data_cell{1,i}{1,3}; % Extract the matrix
+    dataMatrix = data_cell{1,i}{1,4}; % Extract the matrix
     imagesc(dataMatrix); % Heatmap plot
     colorbar; % Add color scale
     caxis([0 1]); % Set colorbar limits from 0 to 1
@@ -22,7 +22,7 @@ numPlots = length(data_RPA_cell); % Number of subplots needed
 for i = 1:numPlots
     % Create a subplot for each matrix
     subplot(ceil(sqrt(numPlots)), ceil(sqrt(numPlots)), i); % Square grid layout
-    dataMatrix = data_RPA_cell{1,i}{1,3}; % Extract the matrix
+    dataMatrix = data_RPA_cell{1,i}{1,4}; % Extract the matrix
     imagesc(dataMatrix); % Heatmap plot
     colorbar; % Add color scale
     caxis([0 1]); % Set colorbar limits from 0 to 1
@@ -32,4 +32,4 @@ for i = 1:numPlots
     set(gca, 'YDir', 'normal'); % Correct Y-axis direction
 end
 
-sgtitle('Heatmaps for data\_cell Matrices'); % Super title for the figure
+sgtitle('Heatmaps for RPA data\_cell Matrices'); % Super title for the figure
--- functions/script_for_plots.m
+++ functions/script_for_plots.m
@@ -22,7 +22,7 @@ numPlots = length(data_RPA_cell); % Number of subplots needed
 for i = 1:numPlots
     % Create a subplot for each matrix
     subplot(ceil(sqrt(numPlots)), ceil(sqrt(numPlots)), i); % Square grid layout
-    dataMatrix = data_RPA_cell{1,i}{1,3}; % Extract the matrix
+    dataMatrix = data_RPA_cell{1,i}{1,subj}; % Extract the matrix
     imagesc(dataMatrix); % Heatmap plot
     colorbar; % Add color scale
     caxis([0 1]); % Set colorbar limits from 0 to 1
@@ -32,7 +32,7 @@ for i = 1:numPlots
     set(gca, 'YDir', 'normal'); % Correct Y-axis direction
 end
 
-sgtitle('Heatmaps for data\_cell Matrices'); % Super title for the figure
+sgtitle(sprintf('Heatmaps for RPA subj %d', subj)); % Super title for the figure
 
 
 
